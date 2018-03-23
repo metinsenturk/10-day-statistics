4460dbab336dfcd59bd07b4eb1dca35025116134
metinsenturk <metinsenturk@me.com>
2018-03-22 23:32:38 -0400

    q8 added

---
+++ q8-least-square-regression.R
@@ -0,0 +1,18 @@
+# Day 8: Least Square Regression Line
+
+txt = "95 85
+85 95
+80 70
+70 65
+60 70"
+input <- read.table(text = txt, header = FALSE)
+x <- input$V1
+y <- input$V2
+xy_model <- lm(y ~ x)
+xy_model_coeffs <- xy_model$coefficients
+
+y_foreighty <- (80 * xy_model_coeffs[2]) + xy_model_coeffs[1]
+
+o <- round(y_foreighty, 3)
+writeLines(as.character(o), stdout())
+
